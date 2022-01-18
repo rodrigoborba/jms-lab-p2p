279aeddc476b7f69d7445621f4f7208f8d1d297d
borba <rodrigoatjoin@gmail.com>
2022-01-18 07:51:04 -0300

    p2p message with listener

---
+++ src/main/java/com/borba/jms/hm/eligibilitycheck/EligibilityCheckerApp.java
@@ -0,0 +1,28 @@
+package com.borba.jms.hm.eligibilitycheck;
+
+import javax.jms.JMSConsumer;
+import javax.jms.JMSContext;
+import javax.jms.Queue;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
+
+public class EligibilityCheckerApp {
+
+	public static void main(String[] args) throws NamingException, InterruptedException {
+		InitialContext initialContext = new InitialContext();
+		Queue requestQueue = (Queue) initialContext.lookup("queue/requestQueue");
+
+		try (ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory();
+				JMSContext jmsContext = cf.createContext("eligibilityuser", "eligibilitypass")) {
+			JMSConsumer consumer = jmsContext.createConsumer(requestQueue);
+			consumer.setMessageListener(new EligibilityCheckListener());
+			
+			Thread.sleep(10000);
+
+		};
+
+	}
+
+}
